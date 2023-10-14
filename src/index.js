f608d7251026f10cf1bbeca2123f32472c624740
Hassan AbuGareeb <Hassan.AbuGareeb@gmail.com>
2023-10-14 21:40:44 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ColoradoStateParks from "./ColoradoStateParks";
+import * as rm from "./parks/RockyMountain";
+
 
 ReactDOM.render(<ColoradoStateParks />, document.getElementById("root"));
